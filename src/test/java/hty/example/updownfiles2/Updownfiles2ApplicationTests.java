f0adba54bc9e7ffcbaf29056d6b2664e5098dba8
bayes <1149144318@qq.com>
2024-04-29 10:26:37 +0800

    [add]:maven配置

---
+++ src/test/java/hty/example/updownfiles2/Updownfiles2ApplicationTests.java
@@ -0,0 +1,20 @@
+package hty.example.updownfiles2;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hty.example.updownfiles2.algorithms.algorithmclassify;
+import hty.example.updownfiles2.entity.person.user;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class Updownfiles2ApplicationTests {
+	//使用字符串进行测试
+
+
+	@Test
+	void contextLoads() {
+
+		//algorithmclassify ay = new algorithmclassify("","","src/main/java/hty/example/updownfiles2/algoconfig/algoclassifytable.json");
+	}
+
+}
